9dd1a651699bb318cc6543461ea336df54186b36
timjinlun <timjinlun@gmail.com>
2023-09-01 18:18:20 +0800

    Add a dog class and dog launcher

---
+++ Add.java
@@ -0,0 +1,10 @@
+public class Add {
+    /** Add the two integers a and b.*/
+    public static int Add(int a, int b){
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Add(5, 10));
+    }
+}
