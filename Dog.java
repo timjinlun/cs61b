e05639eb8cbad88b8b74f23c2d64b7d0a5cf08ea
timjinlun <timjinlun@gmail.com>
2023-09-01 20:38:41 +0800

    Add a instance method for a dog weight comparsion.

--- Dog.java
+++ Dog.java
@@ -14,12 +14,16 @@ public class Dog {
             System.out.println("wwwooooo");
         }
     }
+    /**  A class method which compare the two dog d1 and d2, return the heavier one.*/
     public static Dog maxDog(Dog d1, Dog d2){
         if (d1.weight > d2.weight){
             return d1;
-
         }return d2;
-
         }
-
+    /** A dog instance that could compare with another dog. */
+    public Dog compareDog(Dog d2){
+        if (this.weight > d2.weight){
+            return this;
+        }return d2;
     }
+}
